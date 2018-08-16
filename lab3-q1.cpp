efc1c6157ff28668cb17b2078f7e42a7c9880173
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-08-16 20:50:01 +0530

    Add files via upload

---
+++ lab3-q1.cpp
@@ -0,0 +1,21 @@
+// First include the library
+#include<iostream>
+using namespace std;
+//write the main function
+int main()
+//process
+{
+//declaring variable
+char a='a';
+int b=3;
+float c=4.06;
+double d=5.2333;
+bool e=5844;
+//will get size of variables
+cout <<"size of char;"<<sizeof(char)<<"byte"<<"contents of a="<<a<<endl;
+cout <<"size of int;"<<sizeof(int)<<"byte"<<"contents of b="<<b<<endl;
+cout <<"size of float ;"<<sizeof(float)<<"byte"<<"contents of c="<<c<<endl;
+cout <<"size of double;"<<sizeof(double)<<"byte"<<"contents of d="<<d<<endl;
+cout <<"size of bool;"<<sizeof(bool)<<"byte"<<"contents of e="<<e<<endl;
+return 0;
+}
