b2536e1b40348508fcd80d9abe30ad177c160eed
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-08-17 17:56:00 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,23 @@
+//First include the library
+#include <iostream>
+using namespace std;
+int main() {
+int a,b,sum,subtract,product,remainder; // declaration
+float division;
+a = 9 // definition
+;b = 7 // definition
+;sum = a + b; // adds to numbers
+subtract = a - b; // calculates the difference
+product = a * b; // calculates the product
+division = a / b; // divides the numbers
+remainder = a % b // finds the reminder
+// displaying the calculation in terminal
+;cout << "number are "<< a << "," << b << endl;
+;cout <<"results in integer" << endl;
+;cout << "sum is " << sum << endl;
+;cout << "difference is " << subtract << endl;
+;cout << "product is " << product << endl;
+;cout << "quotient is " << division << endl;
+;cout << "remainder is " <<remainder << endl;
+return 0;
+}
